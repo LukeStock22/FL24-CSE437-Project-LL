96b02ebfbd1174be22cc05a0d1cfa28dbe1318c0
LukeStock22 <l.d.stockbridge@wustl.edu>
2024-09-24 19:05:28 -0500

    edit profile button

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home'; // Importing Home.js correctly
+import EditProfile from './components/EditProfile';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} /> {/* This points to Home.js */}
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
     </Router>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // useNavigate is the new hook
+import { useNavigate, Link } from 'react-router-dom'; // useNavigate is the new hook
 
 const Home = () => {
   const navigate = useNavigate(); // Replacing useHistory with useNavigate
@@ -13,6 +13,9 @@ const Home = () => {
     <div>
       <h2>Welcome to the Home Page</h2>
       <button onClick={handleLogout}>Logout</button>
+      <Link to="/edit-profile">
+        <button>Edit Profile</button>
+      </Link>
     </div>
   );
 };
