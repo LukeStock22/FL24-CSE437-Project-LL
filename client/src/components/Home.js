7afd0b109e4aaf7206d861c4342fde70133457bd
LukeStock22 <l.d.stockbridge@wustl.edu>
2024-10-02 13:26:01 -0500

    Logout from homepage if token expired. Replicate across other pages?

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -26,6 +26,7 @@ const Home = () => {
           setUsername(data.name);
         } else {
           alert('Failed to fetch username');
+          navigate('/');
         }
         setLoading(false);
       })
